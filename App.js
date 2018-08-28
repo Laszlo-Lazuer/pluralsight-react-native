b5463c4ddab6803db775857645055d9dd1d0beb0
Laszlo-Lazuer <visualjazz@gmail.com>
2018-08-28 09:36:36 -0500

    first component - import first component and demo yellowbox warning disabling.

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { YellowBox } from 'react-native';
+import EventList from './EventList';
+
+// YellowBox disable example
+// YellowBox.ignoreWarnings([
+//   'Warning: componentWillMount is deprecated',
+//   'Warning: componentWillReceiveProps is deprecated',
+// ]);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Hello!!</Text>
-      </View>
+      <EventList />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
